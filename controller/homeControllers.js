95f8ae3a45db7498b6aa994f9bb0bba0e48c9a0a
Gabriel Lafrance <gl.gabriellafrance@gmail.com>
2022-03-10 16:33:37 -0500

    Tout Backend fait pour creation account -> login

---
+++ controller/homeControllers.js
@@ -0,0 +1,25 @@
+const axios = require('axios')
+
+exports.renderLogin = (req, res) => {
+    res.render("login.ejs")
+}
+exports.renderProfil = (req, res) => {
+    res.render("profil.ejs")
+}
+exports.renderSignup = (req, res) => {
+    res.render("signup.ejs")
+}
+
+exports.renderNewUser = (req, res) => {
+    let email = req.body.mail
+    let password = req.body.password
+    let username = req.body.userName
+
+    axios.post(`https://ski-api.herokuapp.com/signup`, 
+    {
+        name: username,
+        email: email,
+        password: password
+    }).then(result => {res.render("login", result.data)})
+    .catch()
+}
--- main.js
+++ main.js
@@ -1,1 +1,18 @@
 const express = require ("express");
+const homecontroller = require ("./controller/homeControllers");
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+app.use(express.static("public"))
+
+app.get('/', homecontroller.renderLogin); 
+
+app.get('/signup', homecontroller.renderSignup);
+app.post('/signup', homecontroller.renderNewUser)
+
+app.get('/profil', homecontroller.renderProfil);
+
+
+app.listen(3000, ()=>{console.log("Server runnning on port 3000")})
